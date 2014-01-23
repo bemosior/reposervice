d66648f76b853fa378619c9fd44b859c3ca95dec
Peter Murray <jester@dltj.org>
2014-01-22 21:37:47 -0500

    Add collection description display.

--- lyr_isl_theme_base/islandora_templates/islandora-basic-collection-wrapper.tpl.php
+++ lyr_isl_theme_base/islandora_templates/islandora-basic-collection-wrapper.tpl.php
@@ -11,12 +11,26 @@
 <?php // module_load_include('inc', 'islandora_solr_search', 'blocks'); ?>
 
 <div class="islandora-basic-collection-wrapper">
+  <div id="local-collection-search">
+    <div id="local-search-container">
+      <h2 class="local-collection-search-title">Search this Collection</h2>
 
-  <?php
-    $object_id=$islandora_object->id;
-    print drupal_render(drupal_get_form('islandora_solr_simple_search_form',$object_id));
-  ?>
+      <?php
+        $object_id=$islandora_object->id;
+        print drupal_render(drupal_get_form('islandora_solr_simple_search_form',$object_id));
+      ?>
+    </div>
+  </div>
   
+  <?php if (isset($islandora_object['DESC-TEXT'])): ?>
+    <div class="islandora-basic-collection-description">
+      <?php
+        $description_text = $islandora_object['DESC-TEXT']->content;
+        print check_markup($description_text, 'filtered_html');
+      ?>
+    </div>
+  <?php endif ?>
+
   <div class="islandora-basic-collection clearfix">
     <span class="islandora-basic-collection-display-switch">
       <ul class="links inline">
